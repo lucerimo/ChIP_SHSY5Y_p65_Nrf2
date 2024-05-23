846a53c8969a084e005cf2dfda1245b6045a23a4
lucerimo <luiscesarrm.1019@gmail.com>
2024-05-22 20:58:01 -0600

    Update Diferenciación_celular.R

--- Codigos_R/Diferenciación_celular.R
+++ Codigos_R/Diferenciación_celular.R
@@ -1,17 +1,17 @@
 #############################################################################
-###   An醠isis de diferenciaci髇 #########
-### Se miden 30 c閘ulas de cada condici髇 y se realiza una prueba z########
+###   An谩lisis de diferenciaci贸n #########
+### Se miden 30 c茅lulas de cada condici贸n y se realiza una prueba z########
 #############################################################################
 library(car)
 
-
+#mediciones celulares n=33
 data <- list(
   size_und <- c(183.2,275.8,231.8,271.9,180.5,182.9,323.5,182.7,262.2,365.4,320.2,261.0,229.0,190.4,263.8,164.4,264.7,230.0,314.9,286.5,201.7,329.1,266.1,166.7,237.8,295.4,288.8,231.5,206.1,360.9,195.9,185.6,221.3),
   size_diff <-c(350.5,418.3,363.6,403.1,351.0,576.4,402.8,501.9,484.4,543.5,444.8,578.4,454.6,507.3,418.4,594.1,442.7,442.1,439.5,480.9,377.1,414.4,526.4,429.4,434.1,477.2,478.6,517.6,464.5,545.7,366.6,531.9,557.2)
 )
 
-boxplot(data, names = c("No diferenciada", "Diferenciada"), main="Longitud de las c閘ulas por condici髇", 
-        ylab="Longitud (px)", col = c("#4d4d4dff",  "#5599ffff"),  xlab="Condici髇 celular")
+boxplot(data, names = c("No diferenciada", "Diferenciada"), main="Longitud de las c茅lulas por condici贸n", 
+        ylab="Longitud (px)", col = c("#4d4d4dff",  "#5599ffff"),  xlab="Condici贸n celular")
 
 
 shapiro.test(size_diff)
